64c9c79ed82f1037ec1e978926a31ed3d2a3613d
mahmoud <mahmoud@mahmoud.mahmoud>
2025-04-27 11:32:09 +0300

    api updated

--- src/uniguidecrew/crew.py
+++ src/uniguidecrew/crew.py
@@ -10,7 +10,7 @@ import os
 
 
 # llms
-os.environ['GROQ_API_KEY'] = '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'
+os.environ['GROQ_API_KEY'] = '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'
 llama_llm = LLM(
     model = "groq/llama-3.3-70b-versatile",
     temperature= 0.0
